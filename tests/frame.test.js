0bc3b55d377d12af7d383d38afcf7dd9f2ca1629
Cecily Rowland <cec1rowland@gmail.com>
2023-01-02 15:45:42 +0000

    Created frame class with functions and passing tests

--- tests/frame.test.js
+++ tests/frame.test.js
@@ -1,1 +1,19 @@
 const Frame = require('../lib/frame.js')
+
+describe("Frame", () => {
+  it ("stores roll total as three", () => {
+    const frame = new Frame(1)
+    frame.firstRoll = 1
+    frame.secondRoll = 2
+
+    expect(frame.frameScore()).toEqual(3);
+  })
+
+  it ("stores roll total as ten", () => {
+    const frame = new Frame(1)
+    frame.firstRoll = 4
+    frame.secondRoll = 6
+
+    expect(frame.frameScore()).toEqual(10);
+  })
+});
