2336c00d73a441fb48dd6b1b8d14b49ac22e7c55
Cecily Rowland <cec1rowland@gmail.com>
2023-01-02 17:16:37 +0000

    Created Score class and class functions, successfully testing sum score and total score functions

--- tests/score.test.js
+++ tests/score.test.js
@@ -1,1 +1,33 @@
-const score = require('../lib/score.js')
+const Score = require('../lib/score.js');
+const Frame = require('../lib/frame.js');
+
+describe("Score", () => {
+  it ("combines the scores of two frames and returns a total score of 14", () => {
+    const firstFrame = new Frame(1);
+    firstFrame.firstRoll = 4
+    firstFrame.secondRoll = 5
+
+    const secondFrame = new Frame(2);
+    secondFrame.firstRoll = 1
+    secondFrame.secondRoll = 4
+
+    const frames = [firstFrame, secondFrame];
+    const score = new Score(frames);
+
+    expect(score.totalScore()).toEqual(14);
+  })
+
+  it("returns a value of 36 when player scores a strike", ()  => {
+    const firstFrame = new Frame(1)
+    firstFrame.firstRoll = 10
+
+    const secondFrame = new Frame(2)
+    secondFrame.firstRoll = 2
+    secondFrame.secondRoll = 6
+
+    const frames = [firstFrame, secondFrame];
+    const score = new Score(frames);
+
+    expect(score.totalScore()).toEqual(36);
+  });
+});
